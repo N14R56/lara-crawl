d9c33f12c7d817b6f48f0e7dc974655a59e8664c
Matheus Murray <2.0turbocoder@gmail.com>
2022-10-29 14:53:06 -0300

    feat(/crawl): controller, test, route configs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function crawl(): Response
+    {
+        return response(
+            [
+                0 => [
+                    "code" => "GBP",
+                    "number" => 826,  
+                    "decimal" => 2,  
+                    "currency" => "Libra Esterlina",  
+                    "currency_locations" => [
+                        0 => [
+                            "location" => "Reuno Unido",  
+                            "icon" => "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/22px-Flag_of_the_United_Kingdom.svg.png"
+                        ] ,
+                        1 => [
+                            "location" => "Ilha de Man",  
+                            "icon" => ""
+                        ],
+                        2 => [
+                            "location" => "Guernesey",  
+                            "icon" => "" 
+                        ],
+                        3 => [
+                            "location" => "Jersey",  
+                            "icon" => ""  
+                        ]
+                    ]
+                ]
+            ],
+            200
+        );
+    }
 }
