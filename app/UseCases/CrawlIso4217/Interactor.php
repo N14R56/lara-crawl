ec40d92c07bb8aaba74c63357fd3cd7eddc8bf56
Matheus Murray <2.0turbocoder@gmail.com>
2022-10-30 17:14:03 -0300

    feat: crawl tbody

--- app/UseCases/CrawlIso4217/Interactor.php
+++ app/UseCases/CrawlIso4217/Interactor.php
@@ -4,8 +4,11 @@ namespace App\UseCases\CrawlIso4217;
 
 use App\UseCases\CrawlIso4217\Contracts\InputContract;
 use App\UseCases\CrawlIso4217\Contracts\InteractorContract;
+use DOMDocument;
+use DOMXPath;
 use Goutte\Client;
 use GuzzleHttp\Client as GuzzleHttpClient;
+use Symfony\Component\HttpClient\HttpClient;
 
 class Interactor
 {
@@ -32,10 +35,36 @@ class Interactor
             }
         }
 
-        $httpClient = new Client();
+        $client = new GuzzleHttpClient();
 
-        $response = $httpClient->request('GET', 'https://pt.wikipedia.org/wiki/ISO_4217');
+        $response = $client->get('https://pt.wikipedia.org/wiki/ISO_4217');
 
-        dd($response);
+        $htmlString = (string) $response->getBody();
+
+        //add this line to suppress any warnings
+        libxml_use_internal_errors(true);
+
+        $doc = new DOMDocument();
+
+        $doc->loadHTML($htmlString);
+        
+        $xpath = new DOMXPath($doc);
+
+        $titles = $xpath->evaluate('//*[@id="mw-content-text"]/div[1]/table[3]/tbody');
+        
+        $extractedTitles = [];
+          
+        foreach ($titles as $title) {
+            
+            $sub = substr($title->textContent, 40, 30);
+    
+            dd($sub);
+            
+            $extractedTitles[] = $title->textContent.PHP_EOL;
+
+            echo $title->textContent.PHP_EOL;
+
+        }
+
     }
 }
