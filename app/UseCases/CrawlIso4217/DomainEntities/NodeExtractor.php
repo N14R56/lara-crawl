b5ef401cebb80ef07b7f22cc4d48757bd5eb240b
Matheus Murray <2.0turbocoder@gmail.com>
2022-11-02 13:40:09 -0300

    feat(class): NodeExtractor

---
+++ app/UseCases/CrawlIso4217/DomainEntities/NodeExtractor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\UseCases\CrawlIso4217\BusinessEntities;
+
+use DOMNodeList;
+
+class NodeExtractor
+{
+    public function __construct(DOMNodeList $nodeList)
+    {
+        
+    }
+}
